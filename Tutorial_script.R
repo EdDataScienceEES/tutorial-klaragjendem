65bbb49fcbf7f08495312ed014fd0b9ae7d9ebdc
klaragjendem <s2243021@ed.ac.uk>
2024-11-25 17:43:57 +0000

    first change to README file

--- Tutorial_script.R
+++ Tutorial_script.R
@@ -5,12 +5,14 @@ file.rename(from = "style.css", to = "Misc/style.css")
 file.rename(from = "nc_file_bylakeid.ipynb", to = "Misc/nc_file_bylakeid.ipynb")
 file.rename(from = "LICENSE", to = "Misc/LICENSE")
 file.rename(from = ".gitignore", to = "Misc/.gitignore")
-#file.rename(from = "tut_template.html", to = "Misc/tut_template.html")
-#file.rename(from = "tut_template.md", to = "Misc/tut_template.md")
+file.rename(from = "tut_template.html", to = "Misc/tut_template.html")
+file.rename(from = "tut_template.md", to = "Misc/tut_template.md")
 
 dir.create("figures")
 file.rename(from = "banner.jpg", to = "figures/banner.jpg")
 
-
+dir.create("README")
+file.rename(from = "README.md", to = "README/README.md")
+file.rename(from = "Misc/tut_template.md", to = "tut_template.md")
 
 
